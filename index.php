f20f4ba4fc6b6791144b368375adbb368ea0708a
ViniciusFn <164583834+vinifen@users.noreply.github.com>
2025-05-22 11:33:37 -0300

    finish login and register

---
+++ index.php
@@ -0,0 +1,144 @@
+<?php
+
+$users = [
+    [
+        "id" => 1,
+        "name" => "eu",
+        "password" => "123456"
+    ],
+    [
+        "id" => 2,
+        "name" => "jorge",
+        "password" => "123456"
+    ],
+    [
+        "id" => 3,
+        "name" => "vinicius",
+        "password" => "123456"
+    ]
+];
+
+$sales = [
+    [
+        "id" => 1,
+        "products" => "beringela",
+        "date" => "22/05/2025 14:35:08"
+    ],
+    [
+        "id" => 2,
+        "products" => "carne",
+        "date" => "22/06/2025 11:35:08"
+    ],
+    [
+        "id" => 3,
+        "products" => "arroz, feijao, brocolis",
+        "date" => "21/05/2025 14:35:08"
+    ]
+];
+
+$currentlyUser = null;
+
+function initialScreen(){
+    global $currentlyUser;
+    if(!$currentlyUser){
+        unLoggedScreen();
+    }
+    loggedScreen();
+}
+
+function unLoggedScreen(){
+    echo "Você está deslogado, faça login ou registre-se para usar o sistema." . PHP_EOL;
+    echo "1 - Login" . PHP_EOL;
+    echo "2 - Registre-se" . PHP_EOL;
+    echo "0 - Sair" . PHP_EOL;
+    $option = (int) readline("Escolha uma opção: ");
+    switch ($option) {
+        case 0: 
+            return;
+        case 1:
+            loginScreen();
+            break;
+        case 2:
+            registerScreen();
+            break;
+        default:
+            echo "Inválido";
+            initialScreen();
+            break;
+    }
+}
+
+function loggedScreen(){
+    echo "oi";
+}
+
+function loginScreen(){
+    $name = (string) readline("Nome: ");
+    $password = (string) readline("Senha: ");
+    $result = login($name,$password);
+    if($result['status'] == false){
+        echo "Erro: " . $result['message'] . PHP_EOL;
+        
+    }else{
+        echo $result['message'] . PHP_EOL;
+    }
+    return initialScreen();
+}
+
+function registerScreen(){
+    $name = (string) readline("Nome: ");
+    $password = (string) readline("Senha: ");
+    $passwordConfirm = (string) readline("Confirmar Senha: ");
+    $result = register($name, $password, $passwordConfirm);
+    if(!$result['status']){
+        echo "Erro: " . $result['message'] . PHP_EOL;
+    }else{
+        echo $result['message'] . PHP_EOL;
+    }
+    return initialScreen();
+}
+
+function login(string $name, string $password){
+    global $currentlyUser;
+    if($currentlyUser){
+        return ["status" => false, "message" => "Usuário já logado"];
+    }
+    
+    $userData = getUserDataByName($name);
+    if(!$userData["status"] || $password != $userData["data"]["password"]){
+        return ["status" => false, "message" => "Nome ou senha errados"];
+    }
+    $currentlyUser = ["id"=>$userData["data"]["id"], "name"=>$userData["data"]["name"]];
+    return ["status" => true, "message" => "Login bem sucedido"];
+}
+
+function register(string $name, string $password, string $passwordConfirm){
+    global $users;
+    if($password != $passwordConfirm){
+        return ["status" => false, "message" => "Senhas não são iguais"];
+    }
+    $userData = getUserDataByName($name);
+    if($userData['status']){
+        return ["status"=>false, "message" => "Esse usuário já existe" ];
+    }
+    array_push($users, ["id" => count($users), "name" => $name, "password" => $password]);
+    return ["status" => true, "message" => "Usuário " . $name . " criado com sucesso."];
+}
+
+function getUserDataByName(string $name){
+    global $users;
+    $userData = null;
+    foreach($users as $user){
+        if($user['name'] == $name){
+            $userData = $user;
+            break;
+        }
+    } 
+    if(!$userData) {
+        return ["status" => false, "message" => "Usuário não existe"];
+    }
+    return ["status" => true, "data"=> $userData];
+}
+
+
+initialScreen();
